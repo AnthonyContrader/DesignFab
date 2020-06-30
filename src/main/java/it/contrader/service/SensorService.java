da44e12659233910d565b841b219e424d2e890f7
MarcoC123 <marcocuccolo46@gmail.com>
2020-06-30 16:24:36 +0200

    finita Classe SensorService

--- src/main/java/it/contrader/service/SensorService.java
+++ src/main/java/it/contrader/service/SensorService.java
@@ -1,11 +1,47 @@
 package it.contrader.service;
 
+import java.util.List;
+
 import it.contrader.converter.SensorConverter;
 import it.contrader.dao.SensorDAO;
+import it.contrader.dto.SensorDTO;
+
+
+
 
 public class SensorService {
 	
-	private SensorDAO userDAO;
+	private SensorDAO sensorDAO;
 	private SensorConverter sensorConverter;
+	
+	public SensorService(){
+		this.sensorDAO = new SensorDAO();
+		this.sensorConverter = new SensorConverter();
+	}
+	
 
+	public List<SensorDTO> getAll() {
+		return sensorConverter.toDTOList(sensorDAO.getAllSensor());
+	}
+
+
+	public SensorDTO read(int id) {
+		return sensorConverter.toDTO(sensorDAO.getSensorRead(id));
+	}
+
+
+	public boolean insert(SensorDTO dto) {
+		return sensorDAO.insert(sensorConverter.toEntity(dto));
+	}
+
+
+	public boolean update(SensorDTO dto) {
+		return sensorDAO.update(sensorConverter.toEntity(dto));
+	}
+
+
+	public boolean delete(int id) {
+		return sensorDAO.delete(id);
+	}
+	
 }
