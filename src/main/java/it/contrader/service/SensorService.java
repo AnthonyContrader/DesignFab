4d0c46b0bb58e48c361aabd903e65eb34f0bb6ab
MarcoC123 <marcocuccolo46@gmail.com>
2020-06-30 16:08:33 +0200

    Creata classe SensorConverter

--- src/main/java/it/contrader/service/SensorService.java
+++ src/main/java/it/contrader/service/SensorService.java
@@ -1,11 +1,11 @@
 package it.contrader.service;
 
-import it.contrader.converter.UserConverter;
+import it.contrader.converter.SensorConverter;
 import it.contrader.dao.SensorDAO;
 
 public class SensorService {
 	
 	private SensorDAO userDAO;
-	private UserConverter userConverter;
+	private SensorConverter sensorConverter;
 
 }
