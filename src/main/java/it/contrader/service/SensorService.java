55116e2301c0a8794518a15201ab617dba902ba8
MarcoC123 <marcocuccolo46@gmail.com>
2020-06-30 16:04:17 +0200

    modifica classe SensorService

--- src/main/java/it/contrader/service/SensorService.java
+++ src/main/java/it/contrader/service/SensorService.java
@@ -1,5 +1,11 @@
 package it.contrader.service;
 
+import it.contrader.converter.UserConverter;
+import it.contrader.dao.SensorDAO;
+
 public class SensorService {
+	
+	private SensorDAO userDAO;
+	private UserConverter userConverter;
 
 }
