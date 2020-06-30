ae926a23a7f01c90bfd2a05feee8d77b43962cb6
KarlV1992 <carlo.rocca@hotmail.it>
2020-06-30 16:56:27 +0200

    Create classes MachinesConverter e MaterialsConverter

---
+++ src/main/java/it/contrader/converter/MachinesConverter.java
@@ -0,0 +1,37 @@
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.MachinesDTO;
+import it.contrader.model.Machines;
+
+public class MachinesConverter {
+	public MachinesDTO toDTO(Machines machines) {
+
+		MachinesDTO machinesDTO = new MachinesDTO(machines.getId(), machines.getModel(),machines.getInit_quantity(),machines.getFinal_quantity());
+		return machinesDTO;
+
+	}
+	
+	
+	public Machines toEntity(MachinesDTO machinesDTO) {
+
+		Machines machines = new Machines(machinesDTO.getId(), machinesDTO.getModel(),machinesDTO.getInit_quantity(),machinesDTO.getFinal_quantity());
+		return machines;
+
+	}
+
+	
+
+	public List<MachinesDTO> toDTOList(List<Machines> machinesList) {
+
+		List<MachinesDTO> machinesDTOList = new ArrayList<MachinesDTO>();
+
+		for (Machines machine : machinesList) {
+
+			machinesDTOList.add(toDTO(machine));
+		}
+		return machinesDTOList;
+	}
+}
---
+++ src/main/java/it/contrader/converter/MaterialsConverter.java
@@ -0,0 +1,36 @@
+package it.contrader.converter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import it.contrader.dto.MaterialsDTO;
+import it.contrader.model.Materials;
+
+public class MaterialsConverter {
+
+	public MaterialsDTO toDTO(Materials materials) {
+
+		MaterialsDTO materialsDTO = new MaterialsDTO(materials.getId(), materials.getName());
+		return materialsDTO;
+
+	}
+
+	public Materials toEntity(MaterialsDTO materialsDTO) {
+
+		Materials materials = new Materials(materialsDTO.getMatetialName(), materialsDTO.getid());
+		return materials;
+
+	}
+
+	public List<MaterialsDTO> toDTOList(List<Materials> materialsList) {
+		
+		List<MaterialsDTO> materialsDTOList = new ArrayList<MaterialsDTO>();
+
+		
+		for (Materials materials : materialsList) {
+			
+			materialsDTOList.add(toDTO(materials));
+		}
+		return materialsDTOList;
+	}
+}
