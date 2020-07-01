e2db79cc3b6868df991311440766f4436d4da6e9
MarcoC123 <marcocuccolo46@gmail.com>
2020-07-01 11:03:01 +0200

    Aggiunto attributo id_machine a classi Sensor

--- src/main/java/it/contrader/dto/SensorDTO.java
+++ src/main/java/it/contrader/dto/SensorDTO.java
@@ -3,7 +3,16 @@ package it.contrader.dto;
 public class SensorDTO{
 	private int id;
 	private String sensortype;
+	private int id_machine;
 	
+	public int getId_machine() {
+		return id_machine;
+	}
+
+	public void setId_machine(int id_machine) {
+		this.id_machine = id_machine;
+	}
+
 	public SensorDTO(){
 		
 	}
@@ -15,6 +24,12 @@ public class SensorDTO{
 	public SensorDTO (int id, String sensortype) {
 		this.id = id;
 		this.sensortype = sensortype;
+	}
+	
+	public SensorDTO (int id, String sensortype, int id_machine) {
+		this.id = id;
+		this.sensortype = sensortype;
+		this.id_machine = id_machine;
 	}
 	
 	public int getId() {
@@ -33,7 +48,7 @@ public class SensorDTO{
 	
 	@Override
 	public String toString() {
-		return  id + "\t"  + sensortype;
+		return  id + "\t"  + sensortype + "\t" + id_machine;
 	}
 	
 }
--- src/main/java/it/contrader/model/Sensor.java
+++ src/main/java/it/contrader/model/Sensor.java
@@ -1,9 +1,16 @@
 package it.contrader.model;
 
 public class Sensor {
-	int id;
-	String sensor_type;
+	private int id;
+	private String sensor_type;
+	private int id_machine;
 	
+	public int getId_machine() {
+		return id_machine;
+	}
+	public void setId_machine(int id_machine) {
+		this.id_machine = id_machine;
+	}
 	public int getId() {
 		return id;
 	}
@@ -20,19 +27,30 @@ public class Sensor {
 	
 	public Sensor() {
 	}
+	public Sensor(int id, String sensor_type, int id_machine) {
+		this.id = id;
+		this.sensor_type = sensor_type;
+	}
+	
 	public Sensor(int id, String sensor_type) {
 		this.id = id;
 		this.sensor_type = sensor_type;
 	}
 	
+	public Sensor(String sensor_type, int id_machine) {
+		this.id_machine = id_machine;
+		this.sensor_type = sensor_type;
+	}
 	
 	public Sensor(String sensor_type) {
 		this.sensor_type = sensor_type;
 	}
+
 	@Override
 	public String toString() {
-		return "Sensor [id=" + id + ", sensor_type=" + sensor_type + "]";
+		return "Sensor [id=" + id + ", sensor_type=" + sensor_type + ", id_machine=" + id_machine + "]";
 	}
+
 	
 	@Override
 	public boolean equals(Object obj) {
@@ -46,11 +64,14 @@ public class Sensor {
 		Sensor other = (Sensor) obj;
 		if (id != other.id)
 			return false;
+		if (id_machine != other.id_machine)
+			return false;
 		if (sensor_type == null) 
 			if (other.sensor_type != null)
 				return false;
 	    else if (!sensor_type.equals(other.sensor_type))
 	    	return false;
+		
 	
 		return true;
 	}
