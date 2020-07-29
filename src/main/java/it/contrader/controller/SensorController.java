0a7a93a7756f4718b8cef52792fc53404ddb025d
NicholasTonna93 <nicholas.tonna93@gmail.com>
2020-07-29 14:19:59 +0200

    ciao

--- src/main/java/it/contrader/controller/SensorController.java
+++ src/main/java/it/contrader/controller/SensorController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.contrader.dto.MaterialsDTO;
 import it.contrader.dto.SensorDTO;
+import it.contrader.dto.SensormaterialDTO;
 import it.contrader.service.MaterialsService;
 import it.contrader.service.SensorService;
 
@@ -26,15 +28,14 @@ public class SensorController extends AbstractController<SensorDTO>{
 	private SensorService sensorService;
 	@Autowired
 	private MaterialsService materialsService;
-	@PostMapping("/insertdue/{idMaterial}&{sensor_name}") 
-		public SensorDTO insert2(@PathVariable("sensor_name") String sensor_name,
-				@PathVariable("idMaterial") Long idMaterialFK) {
- 
+	@PostMapping(value ="/insert2") 
+		public SensorDTO insert2( @RequestBody SensormaterialDTO sensormaterialDTO ){
+            System.out.println(sensormaterialDTO.getIdMaterial()+" "+ sensormaterialDTO.getSensor_name());
 			SensorDTO sensorDto = new SensorDTO();
 			MaterialsDTO materialsDTO = new MaterialsDTO();
 
-			materialsDTO = materialsService.read(idMaterialFK);
-			sensorDto.setSensor_name(sensor_name);
+			materialsDTO = materialsService.read(sensormaterialDTO.getIdMaterial());
+			sensorDto.setSensor_name(sensormaterialDTO.getSensor_name());
 			sensorDto.setMaterialsDTO(materialsDTO);		
 			sensorService.insert(sensorDto);
 			return sensorDto;
---
+++ src/main/java/it/contrader/dto/SensormaterialDTO.java
@@ -0,0 +1,24 @@
+package it.contrader.dto;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Non esiste il model associato, serve solo a trasferire un oggetto dal frontend 
+ * che contenga le infrmazioni di username e password.
+ * 
+ * @author Vittorio Valent & Girolamo Murdaca
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SensormaterialDTO {
+	
+	private String sensor_name;
+	
+	private int idMaterial;
+}
+
