8983e33673de67b900b7290fbf2d052b59d9e9fa
NicholasTonna93 <nicholas.tonna93@gmail.com>
2020-07-30 15:44:12 +0200

    user

--- src/main/java/it/contrader/controller/MaterialsController.java
+++ src/main/java/it/contrader/controller/MaterialsController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.contrader.dto.LoginDTO;
 import it.contrader.dto.MaterialsDTO;
+import it.contrader.dto.UserDTO;
 import it.contrader.model.Materials.Materialtype;
 import it.contrader.service.MaterialsService;
 
@@ -26,9 +30,17 @@ public class MaterialsController extends AbstractController<MaterialsDTO> {
 		return ((MaterialsService) this.service).findAllByMaterialType(type);
 
 	}
+	
+
+	@GetMapping(value = "/calcola/{genericQuantity}")
+	public List<MaterialsDTO> calcola(@PathVariable("genericQuantity") double genericQuantity) {
+		
+		
+		return ((MaterialsService) this.service).calcolaService(genericQuantity);
+
 
 	
-	
+	}	
 }
 // con questa chiamata rest possiamo farci una lista di materiali in base al
 // tipo, però in questo caso con il path dinamico in base a come è composto
--- src/main/java/it/contrader/service/MaterialsService.java
+++ src/main/java/it/contrader/service/MaterialsService.java
@@ -26,5 +26,63 @@ public class MaterialsService extends AbstractService<Materials, MaterialsDTO> {
 		
 	}
 	
+	public List<MaterialsDTO> calcolaService(double quantityMaterial){
+		List<MaterialsDTO> list= (List<MaterialsDTO>) this.getAll();
+		System.out.println("listaaaa1" + list);
+		Long id_materialsGenerico = null;
+		Materialtype material_type = null;
+		String material_name = null ;
+		
+		for (MaterialsDTO u : list) {
+			System.out.println(u.getMaterialType());
+			if (u.getMaterialType().toString().equals("GENERIC")) {
+				id_materialsGenerico = u.getIdMaterial();
+				material_type = u.getMaterialType();
+				material_name= u.getMaterialName();
+			}
+		}
+		MaterialsDTO dto = new MaterialsDTO();
+		dto.setMaterialType(material_type);
+		dto.setIdMaterial(id_materialsGenerico);
+		dto.setMaterialsQuantity(quantityMaterial);
+		dto.setMaterialName(material_name);
+		System.out.println("dtoooooo"+dto);
+		this.update(dto);
+		//setAll(request);
+		double quantity = 0;
+		Double Materialsx = quantityMaterial;
+		for (int i = 0; i < list.size() - 1; i++) {
+			if (!list.get(i).getMaterialType().toString().equals("GENERIC") && quantityMaterial > 0 && i == 0) {
 
-}
+				quantity = Math.floor((Math.random() * (Materialsx * 2 / 3 - 0.0) + 0.0));
+				Materialsx -= quantity;
+
+				list.get(i).setMaterialsQuantity(list.get(i).getMaterialsQuantity() + quantity);
+
+				this.update(list.get(i));
+			}
+			if (!list.get(i).getMaterialType().toString().equals("GENERIC") && quantityMaterial > 0) {
+
+				quantity = Math.floor((Math.random() * (Materialsx - 0.0) + 0.0));
+				Materialsx -= quantity;
+
+				list.get(i).setMaterialsQuantity(list.get(i).getMaterialsQuantity() + quantity);
+
+				this.update(list.get(i));
+			}
+
+		}
+		
+		list.get(list.size() - 1).setMaterialsQuantity(Materialsx + list.get(list.size() - 1).getMaterialsQuantity() );
+		this.update(list.get(list.size() - 1));
+		List<MaterialsDTO> finalList= (List<MaterialsDTO>) this.getAll();
+		System.out.println("final list"+finalList);
+		return finalList ;
+
+	}
+
+		
+	}
+	
+
+
